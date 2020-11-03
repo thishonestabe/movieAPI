88d3fca9b00da48b57d42c84633b59d09878c8ff
thishonestabe <marquez.abraham1991@gmail.com>
2020-11-03 06:32:24 +0100

    Added validation of registration with Joi

--- api/auth/auth.routes.js
+++ api/auth/auth.routes.js
@@ -1,20 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../users/users.model');
+const Joi = require('joi');
+
+const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8)
+})
 
 //register
 router.post('/register', async (req, res) => {
-    let user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
-    try {
-        const savedUser = await user.save();
-        res.json(savedUser);
-    }catch (e) {
-        res.status(400).send(e);
+    const {error} = schema.validate(req.body);
+
+    if(!error) {
+        let user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        try {
+            const savedUser = await user.save();
+            res.json(savedUser);
+        }catch (e) {
+            res.status(400).send(e);
+        }
+    } else {
+        res.send(error);
     }
+
 });
 //login
 router.post('/login/',);
