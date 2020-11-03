ef6763e5cd02579a04f65ba6b9d08d5c56d9baae
thishonestabe <marquez.abraham1991@gmail.com>
2020-11-03 06:18:20 +0100

    added unique user model

--- api/auth/auth.routes.js
+++ api/auth/auth.routes.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../users/users.model');
 
 //register
-router.post('/register',);
+router.post('/register', async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+    try {
+        const savedUser = await user.save();
+        res.json(savedUser);
+    }catch (e) {
+        res.status(400).send(e);
+    }
+});
 //login
 router.post('/login/',);
 
