6beb8dc784a0d72903daa0c87b989349c146426d
thishonestabe <marquez.abraham1991@gmail.com>
2020-11-03 09:30:22 +0100

    Added User Routes

--- api/users/users.routes.js
+++ api/users/users.routes.js
@@ -1,16 +1,50 @@
 const express = require('express');
 const router = express.Router();
-
+const User = require('./users.model');
 //get all users.
-router.get('/getusers',);
+router.get('/getusers', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    }catch (err) {
+        res.json({message: err})
+    }
+});
 //get user by id.
-router.get('/getuserbyid/:id',);
-//create a user.
-router.put('/createuser/',);
+router.get('/getuserbyid/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.body.id);
+        res.json(user);
+    }catch (err) {
+        res.json({message: err})
+    }
+});
+
 //edit a user.
-router.patch('/edituser/:id',);
+router.patch('/edituser/:id', async (req, res) => {
+    let updatefields = {}
+    for( const property in req.body) {
+        updatefields[property] = req.body[property];
+    }
+
+    try {
+        const user = await User.updateOne({_id:req.params.id}, {
+            $set: updatefields
+        });
+        res.json(user);
+    }catch (err) {
+        res.json({message: err})
+    }
+});
 //delete a user.
-router.delete('/deleteuser/:id',);
+router.delete('/deleteuser/:id', async (req, res) => {
+    try {
+        const user = await User.deleteOne({_id:req.params.id});
+        res.json(user);
+    }catch (err) {
+        res.json({message: err})
+    }
+});
 
 
 module.exports = router;
